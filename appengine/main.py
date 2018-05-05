da5297a7396a06b46b01ae48bf92a984babf949a
99heitor <99heitor@gmail.com>
2018-05-05 19:40:49 -0300

    PEP8 changes, "/" endpoint changes

--- appengine/main.py
+++ appengine/main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask import jsonify
 import json
 import random
 import requests
@@ -13,15 +12,20 @@ word_dict = {}
 for word in word_list:
     word_dict[word["id"]] = word["palavra"]
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route('/')
+def index():
+    return """Available endpoints: <br>
+                /getRandomWord <br>
+                /getWordByID<id>"""
+
 
 @app.route("/getRandomWord")
 def get_random_word():
     return get_word_by_id(random.choice(word_dict.keys()))
 
-@app.route('/getWordByID/<int:id>')
-def get_word_by_id(id):
-    resp = requests.get('http://www.acessibilidadebrasil.org.br/libras_3/ajax/getWordById/' + str(id))
-    return (resp.content, resp.status_code, resp.headers.items())
+
+@app.route('/getWordByID/<word_id>')
+def get_word_by_id(word_id):
+    resp = requests.get('http://www.acessibilidadebrasil.org.br/libras_3/ajax/getWordById/' + str(word_id))
+    return resp.content, resp.status_code, resp.headers.items()
