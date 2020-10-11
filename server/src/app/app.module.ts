7903070ef089c06131208d35f730f41423da6d3b
MarianaFL <mflgeb@hotmail.com>
2020-10-11 20:25:12 -0300

    Use nest.js to serve static files instead of express

---
+++ server/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { WordModule } from '../words/word.module';
+
+@Module({
+  imports: [
+    WordModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'public'),
+    })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as express from 'express';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('/', express.static('public/'));
   await app.listen(9000);
 }
 bootstrap();
